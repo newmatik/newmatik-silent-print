637dab0e28cc34da611ae6387a288422fe94b119
Shiela Sabellita <shiela.sabellita@gmail.com>
2024-04-01 15:44:27 +0800

    chore: remove connection error pop up

--- silent_print/public/js/silent_print.js
+++ silent_print/public/js/silent_print.js
@@ -1,36 +1,3 @@
-// How to extend page function: https://discuss.erpnext.com/t/pos-custom-script-in-point-of-sale/29554/15
-frappe.require('assets/js/point-of-sale.min.js', function() {
-    erpnext.PointOfSale.PastOrderSummary.prototype.print_receipt =  function(parent) {
-        const frm = this.events.get_frm();
-        var printService = new frappe.silent_print.WebSocketPrinter();
-        frappe.call({
-            method: 'silent_print.utils.print_format.create_pdf',
-            args: {
-                doctype: frm.doc.doctype,
-                name: frm.doc.name,
-                silent_print_format: frm.pos_print_format,
-            },
-            callback: (r) => {
-                if(r.message){
-                    printService.submit({
-                        'type': r.message.print_type,
-                        'url': 'file.pdf',
-                        'file_content': r.message.pdf_base64
-                    });
-                } else {
-                    const frm = this.events.get_frm();
-                    frappe.utils.print(
-                        this.doc.doctype,
-                        this.doc.name,
-                        frm.pos_print_format,
-                        this.doc.letter_head,
-                        this.doc.language || frappe.boot.lang
-                    );
-                }
-            }
-        })
-    }
-})
 
 $(document).on('app_ready', function () {    
 
@@ -125,7 +92,7 @@ frappe.silent_print.WebSocketPrinter = function (options) {
     
     var onError = function () {
         if (frappe.whb == undefined){
-            frappe.msgprint("No se pudo establecer conexión con la impresora. Favor verificar que el <a href='https://github.com/imTigger/webapp-hardware-bridge' target='_blank'>WebApp Hardware Bridge</a> esté ejecutándose.")
+            // frappe.msgprint(__("Could not establish a connection to the printer. Please verify that the <a href='https://github.com/imTigger/webapp-hardware-bridge' target='_blank'>WebApp Hardware Bridge</a> is running."))
             frappe.whb = true
         }
     };
