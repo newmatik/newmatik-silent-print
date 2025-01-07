88646ea8d74ad321a103aafe2c5415257fdfcc41
Sarrah Hillary Grace Macha <shgmacha@gmail.com>
2025-01-07 12:50:30 +0800

    fix: shipment logic change

--- silent_print/public/js/doctypes/shipment.js
+++ silent_print/public/js/doctypes/shipment.js
@@ -1,5 +1,12 @@
 frappe.ui.form.on('Shipment', {
     refresh: function(frm){
-        silent_print.newmatik.add_custom_button(frm, "Shipment")
+        if(frm.doc.status in ["Booked", "Submitted"]){
+            if (!frm.doc.service_provider || !frm.doc.shipment_id) {
+                const missingField = !frm.doc.service_provider ? "Service Provider" : "Shipment ID";
+                frappe.throw(__(`Can't proceed due to the ${missingField} missing field.`));
+            } else {
+                silent_print.newmatik.add_custom_button(frm, "Shipment");
+            }
+        }
     },
 })
